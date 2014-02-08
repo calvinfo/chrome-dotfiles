c87851606d39cc285832f26a7b6962b51e800e86
Tom Brown <tom@joingrouper.com>
2014-02-08 04:05:35 -0500

    Add basic injector tests

--- test/injector_test.js
+++ test/injector_test.js
@@ -1,8 +1,22 @@
 var should = require('should'),
-    gist_loader = require('../lib/injector')
+    Injector = require('../lib/injector')
 
-describe('Load', function() {
-  it("passes", function() {
-    [1,2,3].indexOf(5).should.equal(-1);
+describe("Injector", function() {
+  describe('#cleanGistUrl', function() {
+
+    it("adds raw if we need it", function() {
+      var url = "https://gist.github.com/nottombrown/10dcdfdac7a226971304";
+      Injector.cleanGistUrl(url).should.equal(url + "/raw");
+    });
+
+    it("doesn't add raw if we don't need it", function() {
+      var url = "https://gist.github.com/nottombrown/10dcdfdac7a226971304/raw";
+      Injector.cleanGistUrl(url).should.equal(url);
+    });
+
+    it("returns false when invalid", function() {
+      var url = "http://xkcd.com/1326/";
+      Injector.cleanGistUrl(url).should.equal(false);
+    });
   });
 });
