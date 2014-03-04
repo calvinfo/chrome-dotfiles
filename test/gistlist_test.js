33eb6e42083d19c6ef00217ef77f4ce41f3d8039
Alex Heeton <alex@heeton.me>
2014-03-04 19:43:14 +0000

    add stub gistlist

--- test/gistlist_test.js
+++ test/gistlist_test.js
@@ -1,25 +1,16 @@
 var should = require('should'),
-    Injector = require('../lib/gistlist')
+    GistList = require('../lib/gistlist')
 
-describe("Injector", function() {
-  describe('#scriptUrl', function() {
-
-    it("adds raw if we need it", function() {
-      var url = "https://gist.github.com/nottombrown/10dcdfdac7a226971304";
-      var injector = new Injector(url);
-      injector.scriptUrl.should.equal(url + "/raw");
+describe("GistList", function() {
+  describe('#all', function() {
+    it("is an array", function() {
+      var list = new GistList()
+      list.all().should.be.an.instanceOf(Array);
     });
-
-    it("doesn't add raw if we don't need it", function() {
-      var url = "https://gist.github.com/nottombrown/10dcdfdac7a226971304/raw";
-      var injector = new Injector(url);
-      injector.scriptUrl.should.equal(url);
-    });
-
-    it("returns false when invalid", function() {
-      var url = "http://xkcd.com/1326/";
-      injector = new Injector(url);
-      injector.scriptUrl.should.equal(false);
+    it("is an array of gist objects", function(){
+      var list = new GistList()
+      var gist = list.all()[0];
+      gist.should.have.properties('urlRegex', 'type', 'content');
     });
   });
 });
